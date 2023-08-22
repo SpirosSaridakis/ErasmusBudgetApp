dc3d8eac81f61ea45bc3966b76f416064fbb2292
SpirosSaridakis <spiros.sar1234@hotmail.com>
2023-08-22 16:20:57 +0300

    Now the user is able to open new windows by pressing on the buttons

--- app/src/main/java/com/example/erasmusbudgetapp/MainActivity.kt
+++ app/src/main/java/com/example/erasmusbudgetapp/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.erasmusbudgetapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnExpenseDiary: ImageButton
+    private lateinit var btnStats: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnExpenseDiary = findViewById(R.id.btnExpenseDiary)
+        btnStats = findViewById(R.id.btnStats)
+        btnExpenseDiary.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(p0: View?) {
+                val intent = Intent();
+                intent.setClass(this@MainActivity,ExpenseDiary::class.java);
+                startActivity(intent);
+            }
+        })
+        btnStats.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(p0: View?) {
+                val intent = Intent();
+                intent.setClass(this@MainActivity,Statistics::class.java);
+                startActivity(intent);
+            }
+        })
+
+
+
     }
 }
